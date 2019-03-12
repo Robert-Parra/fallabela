f33e4fcfa7f0884dc2f8ba2861aaa0d4fe4694ac
Robert-Parra <48396536+Robert-Parra@users.noreply.github.com>
2019-03-11 23:14:44 -0500

    Create SQLQuery4.sql
    insert a tablas

---
+++ SQLQuery4.sql
@@ -0,0 +1,11 @@
+
+
+
+INSERT INTO [dbo].[rol]  VALUES ('ADMINISTRADOR'),('CLIENTE')
+INSERT INTO [dbo].[fomapago] VALUES ('EFECTIVO'),('TARJETA')
+INSERT INTO [dbo].[tipodocumento] VALUES ('CEDULA CIUDDANIA')
+ INSERT INTO  [dbo].[COMPAÑIA] values ('Seguros Bolivar','cra45#67-99','3150986789','1234567890', 1)
+ INSERT INTO  [dbo].[producto] values (1,'Seguro Bicicleta',8000000,'cobertura del 80%', 1 )
+ INSERT INTO  [dbo].[persona] values ( 'Don Robert','Parra Polania',1,'1232345672','robertparra300@gmail.com','3022546789',1,'',1, 1 )
+ 
+ 
